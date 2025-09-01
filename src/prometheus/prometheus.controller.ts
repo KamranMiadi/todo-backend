c84cb2893ca0023a0e0fa50f4a20ce783dfafe44
kamran_miadi <kamy.mia74@gmail.com>
2025-09-01 20:11:18 +0330

    debug

--- src/prometheus/prometheus.controller.ts
+++ src/prometheus/prometheus.controller.ts
@@ -3,7 +3,6 @@ import { register } from 'prom-client';
 
 @Controller('metrics')
 export class PrometheusController {
-
   constructor() {
     // Initialize metrics collection
     // You can add more metrics here (e.g., histograms for latency)
