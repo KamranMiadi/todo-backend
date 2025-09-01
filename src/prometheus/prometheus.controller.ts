ce10a2d728433a1b15f5a7e51f785dee8ce2a866
kamran_miadi <kamy.mia74@gmail.com>
2025-09-01 20:10:59 +0330

    debug

--- src/prometheus/prometheus.controller.ts
+++ src/prometheus/prometheus.controller.ts
@@ -1,13 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
-import { register, Counter } from 'prom-client';
+import { register } from 'prom-client';
 
 @Controller('metrics')
 export class PrometheusController {
-  private readonly customHttpRequestsTotal = new Counter({
-    name: 'http_requests_total_custom',
-    help: 'Total number of HTTP requests',
-    labelNames: ['method', 'path', 'status'],
-  });
 
   constructor() {
     // Initialize metrics collection
